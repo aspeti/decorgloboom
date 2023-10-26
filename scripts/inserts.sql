7a4c4e0b9460d05a83d5431badf4e12f2cc612fa
Rodrigo Aspeti <rodrigo.aspeti@meta-dao.com>
2023-10-25 22:49:58 -0400

    set insert values

--- scripts/inserts.sql
+++ scripts/inserts.sql
@@ -1,7 +1,7 @@
-insert into rol (nombre, descripcion, eliminado)values
-("admin","administrador",0),
-("cliente","cliente",0);
+insert into rol (id_rol,nombre, descripcion, eliminado)values
+(1,"admin","administrador",0),
+(2,"Empleado","Empleado",0);
 
-insert into usuario(fecha_creacion,eliminado,email,password,nombre,apellido,id_rol)values
-(CURDATE(),0,"admin@gmail.com",MD5("admin"),'Henry','Aspeti',1),
-(CURDATE(),0,"cliente@gmail.com",MD5("cliente"),'Antonella','Rocusso',2);
+insert into usuario(id_usuario,fecha_creacion,eliminado,email,password,nombre,apellido,id_rol)values
+(1,CURDATE(),0,"admin@gmail.com",MD5("admin"),'Henry','Aspeti',1),
+(2,CURDATE(),0,"cliente@gmail.com",MD5("cliente"),'Antonella','Rocusso',2);
