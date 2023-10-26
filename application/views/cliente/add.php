afa5ca60ef1974a69b69f1b561abb15d92fabd1f
henry <henry.aspeti@gmail.com>
2023-10-26 03:39:40 -0400

    Task009 edited from add cliente

--- application/views/cliente/add.php
+++ application/views/cliente/add.php
@@ -21,7 +21,7 @@
     <section class="content">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-6">
+          <div class="col-8">
 
             <div class="card">
               <!-- /.card-header -->
@@ -46,11 +46,19 @@
               <!-- form start -->
               <form action="<?php echo base_url();?>clientes/insert" method="POST">
                 <div class="card-body">
-                  <div class="form-group ">
+
+                <div class="form-group row">
+                  <div class="form-group  col-md-6 ">
                     <label for="nombre">Nombre *</label>
                     <input type="text" class="form-control <?php echo !empty(form_error("nombre")) ? 'is-invalid':' ';?>" placeholder="nombre" id="nombre" name="nombre" value = "<?php echo set_value("nombre");?>">
                     <?php echo form_error("nombre","<span class='help-block'>","</span>")?>
                   </div>
+                  <div class="form-group col-md-6">
+                    <label for="telefono">Telefono</label>
+                    <input type="text" class="form-control <?php echo !empty(form_error("telefono")) ? 'is-invalid':' ';?>" placeholder="telefono" id="telefono" name="telefono" value = "<?php echo set_value("telefono");?>">
+                    <?php echo form_error("telefono","<span class='help-block'>","</span>")?>
+                  </div>
+                </div>
 
                   <div class="form-group ">
                     <label for="num_documento">Numero Documento</label>
@@ -58,11 +66,7 @@
                     <?php echo form_error("num_documento","<span class='help-block'>","</span>")?>
                   </div>
 
-                  <div class="form-group ">
-                    <label for="telefono">Telefono</label>
-                    <input type="text" class="form-control <?php echo !empty(form_error("telefono")) ? 'is-invalid':' ';?>" placeholder="telefono" id="telefono" name="telefono" value = "<?php echo set_value("telefono");?>">
-                    <?php echo form_error("telefono","<span class='help-block'>","</span>")?>
-                  </div>
+
 
                   <div class="form-group ">
                     <label for="direccion">direccion</label>
