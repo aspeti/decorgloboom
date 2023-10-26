dd7cdcae4e02436d455305edaabe12ec7b0f6d24
Rodrigo Aspeti <rodrigo.aspeti@meta-dao.com>
2023-10-25 23:40:33 -0400

    change name tittle

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="<?php echo base_url();?>assets/img/logo.png" type="image/x-icon">
-  <title>Tienda</title>
+  <title>Decorgloboom</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
