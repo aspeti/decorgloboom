61526f5e0e627dc498ee2cdabaf264f8a2b38364
henry <henry.aspeti@gmail.com>
2023-10-26 03:25:09 -0400

    Task008 edited from edit producto

--- application/views/admin/productos/edit.php
+++ application/views/admin/productos/edit.php
@@ -21,7 +21,7 @@
     <section class="content">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-6">
+          <div class="col-8">
 
             <div class="card">
               <!-- /.card-header -->
@@ -49,36 +49,52 @@
                   <div class="form-group">
                     <input type="hidden" name="idProducto" value="<?php echo $producto->id_producto; ?>">
                   </div>
-                  <div class="form-group">
-                    <label for="codigo">Codigo *</label>
-                    <input type="text" class="form-control <?php echo !empty(form_error("codigo")) ? 'is-invalid':' ';?>" placeholder="codigo" id="codigo" name="codigo" 
-                            value="<?php echo !empty(form_error("codigo")) ? set_value("codigo") : $producto->codigo; ?>">
-                    <?php echo form_error("codigo","<span class='help-block'>","</span>")?>
-                  </div>
-                  <div class="form-group">
-                    <label for="nombre">Nombre *</label>
-                    <input type="text" class="form-control <?php echo !empty(form_error("nombre")) ? 'is-invalid':'';?>" placeholder="nombre" id="nombre" name="nombre"
-                       value="<?php echo !empty(form_error("nombre")) ? set_value("nombre") : $producto->nombre; ?>">
-                    <?php echo form_error("nombre","<span class='help-block'>","</span>")?>   
+                  <div class="form-group row">
+                    <div class="form-group col-md-6">
+                      <label for="codigo">Codigo *</label>
+                      <input type="text" class="form-control <?php echo !empty(form_error("codigo")) ? 'is-invalid':' ';?>" placeholder="codigo" id="codigo" name="codigo" 
+                              value="<?php echo !empty(form_error("codigo")) ? set_value("codigo") : $producto->codigo; ?>">
+                      <?php echo form_error("codigo","<span class='help-block'>","</span>")?>
+                    </div>
+                    <div class="form-group col-md-6">
+                      <label for="nombre">Nombre *</label>
+                      <input type="text" class="form-control <?php echo !empty(form_error("nombre")) ? 'is-invalid':'';?>" placeholder="nombre" id="nombre" name="nombre"
+                        value="<?php echo !empty(form_error("nombre")) ? set_value("nombre") : $producto->nombre; ?>">
+                      <?php echo form_error("nombre","<span class='help-block'>","</span>")?>   
+                    </div>
                   </div>
                   <div class="form-group">
                     <label for="descripcion">Descripcion</label>
                     <input type="text" class="form-control" placeholder="Descripcion" name="descripcion" value="<?php echo $producto->descripcion; ?>">
-                  </div>                 
-                  <div class="form-group">
-                    <label for="precio">Precio *</label>
-                    <input type="text" class="form-control <?php echo !empty(form_error("precio")) ? 'is-invalid':'';?>" placeholder="precio" id="precio" name="precio" 
-                    value="<?php echo !empty(form_error("precio")) ? set_value("precio") : $producto->precio; ?>">
-                    <?php echo form_error("precio","<span class='help-block'>","</span>")?>
                   </div>
+                  <div class="form-group row">                 
+                    <div class="form-group col-md-4">
+                      <label for="precio">Precio *</label>
+                      <input type="text" class="form-control <?php echo !empty(form_error("precio")) ? 'is-invalid':'';?>" placeholder="precio" id="precio" name="precio" 
+                      value="<?php echo !empty(form_error("precio")) ? set_value("precio") : $producto->precio; ?>">
+                      <?php echo form_error("precio","<span class='help-block'>","</span>")?>
+                    </div>
 
-                  <div class="form-group">
-                    <label for="stock">Stock *</label>
-                    <input type="text" class="form-control <?php echo !empty(form_error("stock")) ? 'is-invalid':'';?>" placeholder="Agregue cantidad en Stock" id="stock" name="stock" 
-                    value="<?php echo !empty(form_error("stock")) ? set_value("stock") : $producto->stock; ?>">
-                    <?php echo form_error("stock","<span class='help-block'>","</span>")?>
-                  </div> 
-                  <div class="form-group">
+                    <div class="form-group  col-md-4">
+                      <label for="stock">Stock *</label>
+                      <input type="text" class="form-control <?php echo !empty(form_error("stock")) ? 'is-invalid':'';?>" placeholder="Agregue cantidad en Stock" id="stock" name="stock" 
+                      value="<?php echo !empty(form_error("stock")) ? set_value("stock") : $producto->stock; ?>">
+                      <?php echo form_error("stock","<span class='help-block'>","</span>")?>
+                    </div>
+                    <div class="form-group  col-md-4">                    
+                      <label for="idCategoria">Categoria</label>
+                      <select type="text" class="form-control" placeholder="categoria" name="idCategoria">                      
+                        <?php foreach($categorias as $categoria):?>
+                          <?php if($categoria->id_categoria == $producto->id_categoria):?>
+                                  <option value="<?php echo $categoria->id_categoria?>" selected> <?php echo $categoria->nombre;?></option>
+                          <?php else:?>
+                            <option value="<?php echo $categoria->id_categoria?>"> <?php echo $categoria->nombre;?></option>
+                            <?php endif;?>
+                        <?php endforeach?>
+                      </select>  
+                    </div> 
+                  </div>  
+                  <div class="form-group ">
                     <label for="exampleInputFile">Cargar archivo</label>
                     <div class="input-group">
                       <div class="custom-file">
@@ -88,18 +104,7 @@
                     </div>
                   </div>
 
-                  <div class="form-group">                    
-                    <label for="idCategoria">Categoria</label>
-                    <select type="text" class="form-control" placeholder="categoria" name="idCategoria">                      
-                      <?php foreach($categorias as $categoria):?>
-                        <?php if($categoria->id_categoria == $producto->id_categoria):?>
-                                <option value="<?php echo $categoria->id_categoria?>" selected> <?php echo $categoria->nombre;?></option>
-                        <?php else:?>
-                          <option value="<?php echo $categoria->id_categoria?>"> <?php echo $categoria->nombre;?></option>
-                          <?php endif;?>
-                      <?php endforeach?>
-                    </select>  
-                  </div>            
+           
                 </div>
                 <!-- /.card-body -->
                 <div class="card-footer">
