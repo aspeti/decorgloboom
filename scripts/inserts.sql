573742d93daad264b8d6da64eba46fbddcd83865
Rodrigo Aspeti <rodrigo.aspeti@meta-dao.com>
2023-10-25 23:31:21 -0400

    update data base

--- scripts/inserts.sql
+++ scripts/inserts.sql
@@ -5,3 +5,9 @@ insert into rol (id_rol,nombre, descripcion, eliminado)values
 insert into usuario(id_usuario,fecha_creacion,eliminado,email,password,nombre,apellido,id_rol)values
 (1,CURDATE(),0,"admin@gmail.com",MD5("admin"),'Henry','Aspeti',1),
 (2,CURDATE(),0,"cliente@gmail.com",MD5("cliente"),'Antonella','Rocusso',2);
+
+
+INSERT INTO tipo_cliente (nombre, descripcion) VALUES ('Regular', 'cliente con compras regulares');
+
+
+insert into comprobante (nombre, cantidad,igv,serie) values ('Recibo',1,0,001 );
