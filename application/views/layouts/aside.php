af11841339682f7c5b7246371450be3619b38bc0
henry <henry.aspeti@gmail.com>
2023-11-20 15:18:55 -0400

    Task-updated aside menu

--- application/views/layouts/aside.php
+++ application/views/layouts/aside.php
@@ -1,13 +1,13 @@
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-light-primary elevation-4">
+  <aside class="main-sidebar sidebar-light-primary elevation-4" >
     <!-- Brand Logo -->
     <a href="#" class="brand-link navbar-purple">
       <img src="<?php echo base_url();?>assets/img/logo.png" class="brand-image elevation-0" style="opacity: .8">
       <span class="brand-text font-weight-light" style="color: white;"><strong>DECORGLOBOOM</strong></span>
     </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
+    <!-- Sidebar style="display:none -->
+    <div class="sidebar" >
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -26,7 +26,7 @@
   <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
 
 </head>
-<body class="hold-transition  sidebar-mini sidebar-mini-md layout-fixed layout-navbar-fixed layout-footer-fixed">
+<body class="sidebar-collapse-slides-open sidebar-collapse hold-transition   layout-fixed layout-navbar-fixed layout-footer-fixed" style="height:auto;">
 <div class="wrapper">
 
   <!-- Preloader   
@@ -38,36 +38,93 @@
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-purple">
     <!-- Left navbar links -->
+    <a href="#" class="brand-link navbar-purple">
+      <img src="<?php echo base_url();?>assets/img/logo.png" class="brand-image elevation-0" style="opacity: .8">
+      <span class="brand-text font-weight-light" style="color: white;"><strong>DECORGLOBOOM</strong></span>
+    </a>
     <ul class="navbar-nav">
       <li class="nav-item ">
         <a class="nav-link navbar-purple" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li> 
     </ul>
 
+    
+
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
+
+
+
+      <ul class="navbar-nav">
+      <?php if($this->session->userdata('rol') == 1) { ?>  
+        <li class="nav-item dropdown">
+          <a class="nav-link navbar-purple" data-toggle="dropdown" href="#">
+            Administrar
+            <i class="nav-icon fa fa-cubes"></i>
+          </a>
+          <div class="dropdown-menu">
+            <a href="<?php echo base_url();?>usuarios" class="dropdown-item">
+              <i class="fas fa-users nav-icon"></i>
+              Usuarios
+            </a>
+            <a href="<?php echo base_url();?>categorias" class="dropdown-item">
+              <i class="fa fa-tags nav-icon "></i>
+              Categorias
+            </a>
+            <a href="<?php echo base_url();?>productos" class="dropdown-item">
+              <i class="fa fa-window-restore nav-icon"></i>
+              Productos
+            </a>
+          </div>
+        </li>           
+      <?php }?>                        
+      <?php if($this->session->userdata('login')) { ?>   
+        <li class="nav-item">
+          <a href="<?php echo base_url();?>clientes" class="nav-link navbar-purple">
+            <p>Clientes</p>
+          </a>
+        </li>   
+        <li class="nav-item">
+          <a href="<?php echo base_url();?>ventas" class="nav-link navbar-purple">
+            <p>Ventas</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="<?php echo base_url();?>reportes" class="nav-link navbar-purple">
+            <p>Reportes</p>
+          </a>
+        </li> 
+        <li class="nav-item">
+          <a href="<?php echo base_url();?>reportes/estadistica" class="nav-link navbar-purple">
+            <p>Mas Vendidos</p>
+          </a>
+        </li>    
+      <?php }?>     
+      </ul>
       <!-- Navbar Search -->    
        <!-- Notifications Dropdown Menu -->
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
+          
           <i class="fa fa-user navbar-purple"></i>
           <!--span class="badge badge-warning navbar-badge">15</!--span-->
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-item dropdown-header">Mi Perfil</span>
+          <span class="dropdown-item dropdown-header"> Perfil</span>
           <div class="dropdown-divider"></div>
           <a href="<?php echo base_url()?>usuarios/viewprofile/<?php echo $this->session->userdata('id_usuario'); ?> " class="dropdown-item">
-            <i class="fa fa fa-user mr-2"></i>Mis datos
+            <i class="fa fa fa-user mr-2"></i>
+            <?php echo $this->session->userdata("nombre");?>
             <!--span class="float-right text-muted text-sm">3 mins</!span-->
           </a>
           <div class="dropdown-divider"></div>
           <a href="<?php echo base_url('usuarios/password/'.$this->session->userdata('id_usuario')); ?>" class="dropdown-item">
-            <i class="fa-solid fa-lock mr-2"></i> Actualizar Contaseña
+            <i class="fa-solid fa-lock mr-2"></i> Cambiar Contraseña
             <!--span-- class="float-right text-muted text-sm">12 hours</!--span-->
           </a>        
           <div class="dropdown-divider"></div>
           <a href="<?php echo base_url();?>/auth/logout" class="dropdown-item ">
-          <i class="fa-solid fa-right-from-bracket fa-lg mr-2"></i></ion-icon>Cerrar Session</a>
+          <i class="fa-solid fa-right-from-bracket fa-lg mr-2"></i></ion-icon>Salir</a>
         </div>
       </li>
 
